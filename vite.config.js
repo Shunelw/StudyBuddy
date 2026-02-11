3a6babe83e4cfe62f2b54b256ffd18ea0a625372
Shune Lai Wai <shunelaiwai@Shunes-Laptop.local>
2026-02-12 00:00:46 +0700

    Second commit

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/StudyBuddy_deploy/',
   plugins: [react()],
-  server: {
-    // port: 3000,
-    port: 5173,
-  },
 })
